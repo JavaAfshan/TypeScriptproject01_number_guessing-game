b26f6619b4da5bacc4d82377f805f18cffa7f6b7
Afshan Yasmeen <115390656+JavaAfshan@users.noreply.github.com>
2023-02-19 03:21:23 -0800

    TypeScriptproject01_number_guessing-game
    Number Guessing Game CLI in TypeScript

---
+++ guessNumber.js
@@ -0,0 +1,28 @@
+#!/usr/bin/env node
+import * as readline from 'readline';
+const randomNumber = Math.floor(Math.random() * 10) + 1;
+const r1 = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+function guessNumberGame(guessedNumber) {
+    const numberGuessed = parseInt(guessedNumber);
+    if (isNaN(numberGuessed)) {
+        console.log("Please enter the number ");
+    }
+    else {
+        if (numberGuessed == randomNumber) {
+            console.log("Hey you are genius you guess the correct number");
+            r1.close();
+        }
+        else if (numberGuessed < randomNumber) {
+            console.log("You guess is too low Guess again: ");
+        }
+        else if (numberGuessed > randomNumber) {
+            console.log("You guess is too high Guess again: ");
+        }
+    }
+}
+console.log("Guess the number between 1 and 10");
+r1.prompt();
+r1.on('line', guessNumberGame);
